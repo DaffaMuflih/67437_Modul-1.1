2b0d484de7aa2aea3bcac976deebfb737fe20b3b
DaffaMuflih <daffa.muflih02@gmail.com>
2025-02-06 09:24:22 +0700

    Menata gaya dan tema aplikasi

--- app/src/main/java/com/example/a67437_modul11/MainActivity.kt
+++ app/src/main/java/com/example/a67437_modul11/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.a67437_modul11
 
+import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -26,6 +27,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.a67437_modul11.ui.theme._67437_Modul11Theme
@@ -119,7 +121,12 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
                 .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello, ")
-                Text(text = name)
+                Text(
+                    text = name,
+                    style = MaterialTheme.typography.headlineMedium.copy (
+                        fontWeight = FontWeight.ExtraBold
+                    )
+                )
             }
             ElevatedButton(
                 onClick = { expanded = !expanded }
@@ -130,7 +137,12 @@ private fun Greeting(name: String, modifier: Modifier = Modifier) {
     }
 }
 
-@Preview(showBackground = true, widthDp = 320)
+@Preview(
+    showBackground = true,
+    widthDp = 320,
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "GreetingPreviewDark"
+)
 @Composable
 fun GreetingPreview() {
     _67437_Modul11Theme {
